fbc296572be6339527fbc1040d87c51f47e0b106
Ju <julienbmartel@gmail.com>
2022-10-12 15:49:01 -0500

    cors again

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require("express")();
+// const cors = require('cors')
 const { getSrc, setSrc, scrapeImgUrl } = require("./lib/utils")
 
 app.post("/refetch", async (req, res) => {
@@ -12,7 +13,15 @@ app.post("/refetch", async (req, res) => {
   }
 })
 
-app.get("/", async (req, res) => {
+app.get("/",async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
   try {
     const src = await getSrc()
     res.status(200).json({ src })
