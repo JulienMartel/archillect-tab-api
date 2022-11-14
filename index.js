f52c6c11ef420268b67400f4c589dba0205bbe9f
ju <julienbmartel@gmail.com>
2022-11-14 01:49:22 -0600

    removed /refresh

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
 const app = require("express")();
-// const cors = require('cors')
-const { getSrc, setSrc, scrapeImgUrl } = require("./lib/utils")
+const { getSrc } = require("./lib/utils");
 
-app.post("/refetch", async (req, res) => {
-  try {
-    const src = await scrapeImgUrl()
-    await setSrc(src)
-
-    res.status(200).json({ success: true });
-  } catch (err) {
-    return res.status(500).json({ statusCode: 500, message: err.message })
-  }
-})
-
-app.get("/",async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+app.get("/", async (_, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
 
   try {
-    const src = await getSrc()
-    res.status(200).json({ src })
-    
+    const src = await getSrc();
+    res.status(200).json({ src });
   } catch (err) {
-    return res.status(500).json({ statusCode: 500, message: err.message })
+    return res.status(500).json({ statusCode: 500, message: err.message });
   }
-})
+});
 
-app.listen(process.env.PORT)
-module.exports = app
+app.listen(process.env.PORT);
+
+module.exports = app;
