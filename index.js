30e3c291b6465c43cdcd96a3a3dc5d84c0f252d2
ju <julienbmartel@gmail.com>
2022-12-02 19:09:39 -0600

    add: b64 query param

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
+// @ts-nocheck
 const path = require("node:path");
 const app = require("express")();
 const { getRandom, getTv, getPost, getRecent } = require("./lib/utils");
 const { client } = require("./lib/mongodb");
 const cors = require("cors");
 
+const ifBase64 = async (img, isBase64, isGif) => {
+  if (!isBase64) return img;
+
+  const res = await fetch(img.src);
+  const buffer = Buffer.from(await res.arrayBuffer());
+  const src =
+    `data:image/${isGif ? "gif" : "jpg"};base64,` + buffer.toString("base64");
+
+  return { ...img, src };
+};
+
 app.use(cors());
 
 app.use(async (req, res, next) => {
   await client.connect();
+  next();
+});
+
+app.use((req, _, next) => {
+  req.isBase64 = req.query.b64 === "true";
   next();
 });
 
@@ -19,18 +36,22 @@ app.get("/", async (_, res) => {
   }
 });
 
-app.get("/random", async (_, res) => {
+app.get("/random", async (req, res) => {
   try {
-    const img = await getRandom();
+    let img = await getRandom();
+    img = await ifBase64(img, req.isBase64);
+
     res.status(200).json(img);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-app.get("/tv", async (_, res) => {
+app.get("/tv", async (req, res) => {
   try {
-    const gif = await getTv();
+    let gif = await getTv();
+    gif = await ifBase64(gif, req.isBase64, true);
+
     res.status(200).json(gif);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -39,7 +60,9 @@ app.get("/tv", async (_, res) => {
 
 app.get("/post/:postId", async (req, res) => {
   try {
-    const img = await getPost(Number(req.params.postId));
+    let img = await getPost(Number(req.params.postId));
+    img = await ifBase64(img, req.isBase64);
+
     res.status(200).json(img);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -49,7 +72,10 @@ app.get("/post/:postId", async (req, res) => {
 app.get("/recent/:count", async (req, res) => {
   try {
     const count = Number(req.params?.count);
-    const imgs = await getRecent(count);
+
+    let imgs = await getRecent(count);
+    imgs = await Promise.all(imgs.map((img) => ifBase64(img, req.isBase64)));
+
     res.status(200).json(imgs);
   } catch (error) {
     res.status(500).json({ error: error.message });
